6a4a30c606b2f97e50ad6c04b985e0719b0e88bb
Sofya <vampiresszero@gmail.com>
2021-12-03 23:18:02 +0700

    all html file

---
+++ AMR_Project/Migrations/20211203153657_Intital.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AMR_Project.Migrations
+{
+    public partial class Intital : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
