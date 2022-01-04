92e4ccbf65f0e1d8b9d8beac319f453422ee50a1
Sofya <vampiresszero@gmail.com>
2022-01-04 23:00:29 +0700

    add modal window

--- AMR_Project/wwwroot/js/script.js
+++ AMR_Project/wwwroot/js/script.js
@@ -1,17 +1,16 @@
 "use strict";
-let //ongoingsWeekDayItem = document.querySelectorAll(".ongoingsWeekDay-item"),
-    divWeekDay = document.querySelector(".WeekDay"),
+let divWeekDay = document.querySelector(".WeekDay"),
     ongoingsWeekDay = document.querySelectorAll(".ongoingsWeekDay"),
     nameWeekDay = document.querySelectorAll(".nameWeekDay");
 divWeekDay.addEventListener('click', function(event){
     let target = event.target;
     if (target && target.classList.contains('nameWeekDay')){
         for (let i = 0; i < nameWeekDay.length; i++){
-            if (target == nameWeekDay[i]  && ongoingsWeekDay[i].classList.contains('displayNone')){
+            if (target == nameWeekDay[i] && ongoingsWeekDay[i].classList.contains('displayNone')){
                 ongoingsWeekDay[i].classList.remove('displayNone');
                 ongoingsWeekDay[i].classList.add('displayInline-block');
                 break;
-            }else if (target == nameWeekDay[i]  && ongoingsWeekDay[i].classList.contains('displayInline-block')){
+            }else if (target == nameWeekDay[i] && ongoingsWeekDay[i].classList.contains('displayInline-block')){
                 ongoingsWeekDay[i].classList.remove('displayInline-block');
                 ongoingsWeekDay[i].classList.add('displayNone');
                 break;
@@ -22,3 +21,29 @@ divWeekDay.addEventListener('click', function(event){
     }
 });
 
+let divAuthorization = document.querySelector('.sectionAuthorization'),
+    popupCloseA = document.querySelector('.popup-close'),
+    authorization = document.getElementById('authorization'),
+    divRegistration = document.querySelector('.sectionRegistration'),
+    popupCloseR = document.querySelectorAll('.popup-close')[1],
+    registration = document.getElementById('registration');
+
+console.log(popupCloseR);
+function popupWindow (clickButton, window, popupClose){
+    clickButton.addEventListener('click', function(){
+        window.classList.remove('displayNone');
+    });
+    popupClose.addEventListener('click', function(){
+        window.classList.add('displayNone');
+    })
+
+}
+// function popupWindowClose (clickButton, window, popupClose){
+//     clickButton.addEventListener('click', function(){
+//         window.classList.remove('displayNone');
+//     })
+// }
+
+popupWindow(authorization, divAuthorization, popupCloseA);
+
+popupWindow(registration, divRegistration, popupCloseR);
