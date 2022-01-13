6b9ff54b9788b41effb69c55ae61f101b815e332
Alexander <Joker124rus@gmail.com>
2022-01-13 21:54:31 +0700

    Ranobe reading page

--- AMR_Project/Pages/Ranobes/RanobeRead.cshtml.cs
+++ AMR_Project/Pages/Ranobes/RanobeRead.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using AMR_Project.DAL;
+using AMR_Project.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +12,21 @@ namespace AMR_Project.Pages.Ranobes
 {
     public class RanobeReadModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationContext _db;
+
+        public RanobeReadModel(ApplicationContext db)
         {
+            _db = db;
+        }
+
+        public Ranobe Ranobe { get; set; }
+        public RanobePage RanobePage { get; set; }
+        public void OnGet(Int32 ranobeId, Int32 chapterNumber)
+        {
+            Ranobe = _db.Find<Ranobe>(ranobeId);
+            _db.Entry(Ranobe).Collection(r => r.RanobePages).Load();
+            RanobePage = Ranobe.RanobePages.Find(p => p.ChapterNumber == chapterNumber);
+
         }
     }
 }
